e0bb63200a0d1f00473ef20d2c91d03bd830d63f
sauravsamui <samuisaurav1@gmail.com>
2022-07-20 14:39:20 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@
 import './App.css';
 import {Routes,Route} from "react-router-dom";
 import Navbar from './component/Navbar/Navbar';
-import Question from './Pages/Question/Question';
-import Home from './Pages/Question/Home';
-import Weight from './Pages/Weight/Weight';
+//import Question from './Pages/Question/Question';
+//import Home from './Pages/Question/Home';
+//import Weight from './Pages/Weight/Weight';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Routes>
-      <Route path='/' element={<Home/>}/>
+      {/* <Route path='/' element={<Home/>}/>
       <Route path='/question' element={<Question/>}/>
-      <Route path='/weight' element={<Weight/>}/>
+      <Route path='/weight' element={<Weight/>}/> */}
 
 
       </Routes>
