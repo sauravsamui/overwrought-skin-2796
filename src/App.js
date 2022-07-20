7090eb3de8b51af304f478bd5811804af79ef19f
Debasree-3031999 <debasreeghosh1999@gmail.com>
2022-07-20 17:56:07 +0530

    Homepage pull merge

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Navbar from './component/Navbar/Navbar';
 import Question from './Pages/Question/Question';
 import Home from './Pages/Question/Home';
 import Weight from './Pages/Weight/Weight';
+import Homepage from './Pages/LandingPage/Landingpage';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route path='/' element={<Homepage/>}/>
+      {/* <Route path='/' element={<Home/>}/> */}
       <Route path='/question' element={<Question/>}/>
       <Route path='/weight' element={<Weight/>}/>
 
--- src/Pages/HomePage/Homepage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Homepage from './pages/Homepage/Homepage.js'
-
-export default function Homepage() {
-  return (
-    <div><Homepage/></div>
-  )
-}
